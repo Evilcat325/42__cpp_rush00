3ba37f1d5463ac73f5ba502865e0c936286c36d6
Sen Li <seli@e1z3r4p24.42.us.org>
2019-05-05 13:17:49 -0700

    base player class

---
+++ src/Player.cpp
@@ -0,0 +1,17 @@
+#include "Player.hpp"
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+Player::Player(Player const &rhs)
+{
+}
+
+Player &Player::operator=(Player const &rhs)
+{
+}
---
+++ src/Player.hpp
@@ -0,0 +1,15 @@
+#ifndef FT_PLAYER_HPP
+#define FT_PLAYER_HPP
+
+class Player
+{
+private:
+	/* data */
+public:
+	Player();
+	~Player();
+	Player(Player const &rhs);
+	Player &operator=(Player const &rhs);
+};
+
+#endif /* FT_Player_HPP */
