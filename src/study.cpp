7040028984166cfd84509320b924629ee16a53d1
Sen Li <seli@e1z3r4p24.42.us.org>
2019-05-05 12:05:36 -0700

    auto formating

--- src/study.cpp
+++ src/study.cpp
@@ -7,18 +7,17 @@ int main()
         y0 = 5;
         nlines = 500;
         ncols = 500;
-        initscr();                      /* Start curses mode            */
+        initscr(); /* Start curses mode            */
         cbreak();
-        keypad(stdscr, TRUE);           /* We get F1, F2 etc..          */
-        noecho();                       /* Don't echo() while we do getch */
-        WINDOW * win = newwin(nlines, ncols, y0, x0);
-
+        keypad(stdscr, TRUE); /* We get F1, F2 etc..          */
+        noecho();             /* Don't echo() while we do getch */
+        WINDOW *win = newwin(nlines, ncols, y0, x0);
 
         wrefresh(win);
 
-        refresh();                      /* Print it on to the real screen */
-        getch();                        /* Wait for user input */
-        endwin();                       /* End curses mode                */
+        refresh(); /* Print it on to the real screen */
+        getch();   /* Wait for user input */
+        endwin();  /* End curses mode                */
 
         return 0;
 }
